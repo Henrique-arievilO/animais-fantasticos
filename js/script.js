a9ff8f696b5a9ec1851d39612b4191334ef96247
Henrique Oliveira <wandholiveira1@gmail.com>
2026-07-05 02:07:14 -0300

    :arrows_clockwise: Change the initAnime Scrollposition

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import SmoothScroll from "./modules/initSmoothScroll.js";
-import initTabNav from "./modules/initTabNav.js";
-import initAnimeScroll from "./modules/initAnimeScroll.js";
 import initAccordion from "./modules/initAccordion.js";
+import initTabNav from "./modules/initTabNav.js";
 import initModal from "./modules/initModal.js";
 import initTooltip from "./modules/initTooltip.js";
 import initDropdownMenu from "./modules/initDropdownMenu.js";
@@ -9,15 +8,13 @@ import initMenuMobile from "./modules/initMenuMobile.js";
 import initOperation from "./modules/initOperation.js";
 import initFetchAnimais from "./modules/initFetchAnimais.js";
 import initFetchBitcoin from "./modules/initFetchBitcoin.js";
-// import $ from "jquery";
-// import _ from "lodash";
+import initAnimeScroll from "./modules/initAnimeScroll.js";
 
 const smoothScroll = new SmoothScroll('[data-menu="smooth"] a[href^="#"]');
 smoothScroll.init();
 
 initAccordion();
 initTabNav();
-initAnimeScroll();
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -25,3 +22,4 @@ initMenuMobile();
 initOperation();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimeScroll();
